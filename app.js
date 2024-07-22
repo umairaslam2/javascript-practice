eeb0f3e6370b6eb12be049eac04e0752f3ea6b1d
umairaslam2 <umairaslamumer@gmail.com>
2024-07-22 12:05:21 +0500

    add if statement & var

--- app.js
+++ app.js
@@ -260,3 +260,63 @@
     //     var name = "aslam";
         
     //     console.log("outer name-->", name);
+
+
+    // if(cityName === "karachi"){
+//   alert("welcome to the city of lights")
+// }else{
+//   alert("wrong city entered")
+// }
+// var gender = prompt("enter your gender")
+// if(gender === "male"){
+// alert("Good Morning Sir")
+// }else(alert("Good Morning Madam"))
+// var signalColor = prompt("enter color name to know alerts")
+// if(signalColor === "red"){
+//   alert("Must Stop")
+// }
+// if(signalColor === "yellow"){
+//   alert("Ready to Move")
+// }
+
+// if(signalColor === "green"){
+//   alert("move now")
+// }
+
+// var fuel = prompt("how many fuel left your vehicle in litres")
+// if(fuel <= 0.25){
+//   alert("“Please refill the fuel in your car”")
+// }else{alert("dont Worry and go on")}
+
+// var a = 4;
+// if (++a === 5){
+//   alert("given condition for variable a is true");
+//   }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }else{alert("given condition for variable b is false");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals " + totalCost);
+// }
+// if("car" < "cat"){
+//   alert("car is smaller than cat");
+//   }
