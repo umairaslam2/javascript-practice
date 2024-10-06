7537a0b4ef481e3e8b9ef1a7e168145fd117709f
umairaslam2 <umairaslamumer@gmail.com>
2024-10-06 19:34:41 +0500

    palindrome or not

--- app.js
+++ app.js
@@ -2025,6 +2025,18 @@
 // console.log(reverseKaro(12));
 
 
+// 7 write a javascript function tat check if  number is palindrome or not;
+
+
+// function palindromCheck(str){
+//   var reversed =  str.split("").reverse().join("");
+
+//   if(reversed === str) return true;
+//   else return false;
+// }
+
+// console.log(palindromCheck("lool"))
+
 
 
 
