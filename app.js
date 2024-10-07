bf6bccc9baec1a414c0ea942735b8c12c8b49a6d
umairaslam2 <umairaslamumer@gmail.com>
2024-10-07 12:57:13 +0500

    function that return each  letter occurrences

--- app.js
+++ app.js
@@ -2060,6 +2060,25 @@
 
 // console.log(capitilizrKaro("umair kese"))
 
+// 10. write a javascript function that return each  letter occurrences 
+
+// function occ(str){
+//     var occurrence = {};
+//    str.split("").forEach(function(elem) {
+//     if(occurrence.hasOwnProperty(elem) === false){
+//         occurrence[elem] = 1;
+//     }
+
+//     else{
+//         occurrence[elem]++;
+//     }
+//    });
+
+//    return occurrence;
+// }
+
+// console.log(occ("apple"))
+
 
 
 
