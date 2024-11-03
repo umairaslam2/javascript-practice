54d2869f9e0c9264bcc4253b5c0385a5e33ffd2d
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2024-11-03 23:45:02 +0500

    add

--- app.js
+++ app.js
@@ -2436,6 +2436,28 @@
 //    console.log(city) 
 
 
+//function
+
+// function calculate(a, b) {
+//     const add = a + b;
+//     const subtract = a - b;
+//     const multiply = a * b;
+//     const divide = a / b;
+  
+//     return [add, subtract, multiply, divide];
+//   }
+  
+//   const [add, subtract, multiply, divide] = calculate(4, 7);
+  
+//   console.log("Sum: " + add);
+//   console.log("Difference " + subtract);
+//   console.log("Product: " + multiply);
+//   console.log("Quotient " + divide);
+
+
+
+
+
 
 
 
