44d2d62a1f5a99c951a168dbfb46785ca9ec50c3
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2024-11-30 22:36:58 +0500

    add this

--- app.js
+++ app.js
@@ -2889,4 +2889,18 @@
 // console.log(newStudents)
 
 
+// Anagrams:
+
+// function groupAnagrams(words) {
+//     return Object.values(words.reduce((acc, word) => {
+//         const key = word.split('').sort().join('');
+//         (acc[key] = acc[key] || []).push(word);
+//         return acc;
+//     }, {}));
+// }
+
+// const input = ["bat", "tap", "cat", "tab", "pat"];
+// console.log(groupAnagrams(input));
+
+
 
