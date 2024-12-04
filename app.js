2e5c166e7cfe305db65442809822e8432a62a51f
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2024-12-04 23:45:40 +0500

    sort of Array

--- app.js
+++ app.js
@@ -3040,3 +3040,25 @@
   //? If all three sides are of equal length, return "equilateral".
   //? If only two sides are of equal length, return "isosceles".
   //? If all three sides have different lengths, return "scalene".
+
+  
+  //*-------------------------------------------
+//*  Programming Question: Sort an Array
+//*-------------------------------------------
+
+// ? Write a function to sort an array of numbers in an ascending order.
+
+// const sortAscending = (arr) => {
+//   return arr.sort((a, b) => b - a);
+// };
+
+// Example usage:
+// console.log(sortAscending([5, 3, 1, 8])); // Output: [1, 3, 5, 8]
+//console.log(sortAscending([5, 3, 10, 8])); // Output: [3, 5, 8,10]
+
+//todo Requirements:
+//? The function should take an array of numbers as input.
+//? It should return a new array with the numbers sorted in ascending order.
+//? The original array should remain unchanged.
+//? You are not allowed to use the built-in sort() 
+
