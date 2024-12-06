08e11ac6b7251e86831eb7b70d1b069cf3019c3a
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2024-12-06 20:50:56 +0500

    convert a string to camelCase & snake_case.

--- app.js
+++ app.js
@@ -3120,13 +3120,13 @@
 //*--------------------------------
 //? 17: Write a function to check if a character is uppercase or lowercase.
 
-const isUpperCase = (char) => {
+//const isUpperCase = (char) => {
   //   if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
   //     return true;
   //   }
   //   return false;
-  return char === char.toUpperCase();
-};
+//   return char === char.toUpperCase();
+// };
 
 // I am just testing is all right
 
@@ -3135,9 +3135,36 @@ const isUpperCase = (char) => {
 //? The character can be any printable ASCII character.
 //? You can assume that the input will always be a string of length 1.
 // Example usage:
-console.log(isUpperCase("S")); // Output: true
+//console.log(isUpperCase("S")); // Output: true
 // console.log(isLowerCase("b")); // Output: true
 
 //* Notes:
 //? Ensure that the function correctly identifies uppercase characters based on their ASCII values.
 //? Optimize the function to handle edge cases efficiently.
+
+
+//*--------------------------------
+//* Coding Challenge
+//*--------------------------------
+//? 18: Write a function to convert a string to camelCase & snake_case.
+
+// const toCamelCase = (str) => {
+//   return str
+//     .trim()
+//     .split(" ")
+//     .map((curElem, index) => {
+//       if (index === 0) {
+//         return curElem.toLowerCase();
+//       } else {
+//         return curElem.charAt(0).toUpperCase() + curElem.slice(1).toLowerCase();
+//       }
+//     })
+//     .join("");
+// };
+
+// Example usage:
+//console.log(toCamelCase("hello world thApa")); // Output: helloWorld
+
+//todo HomeWork:
+// console.log(toSnakeCase("helloWorld")); // Output: hello_world
+
