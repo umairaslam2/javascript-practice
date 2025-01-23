8802655e98802ecf1ae703e5e2ab43eb3ca5058f
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-01-23 21:50:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4694,3 +4694,45 @@ const filterSearch = (value) => {
 
 
 
+
+// destructuring;
+
+
+// let student = {
+//     name: "Umair",
+//     email: "umair@company.com"
+//     classDetails: {
+//         time: "9-11";
+//     }
+// }
+
+// let time = "1212";
+
+
+// let {name, email, classDetails} = student;
+// let{time: classTime} = classDetails;
+
+// console.log(name)
+// console.log(email)
+// console.log(time)
+
+
+// Default parameters;
+
+// let student = {
+//     name: "Umair",
+//     email: "umair@company.com"
+//     classDetails: {
+//         time: "subha";
+//     }
+// }
+
+// let time = "1212";
+
+
+// let {name, email, classDetails} = student;
+// let {time = "9-11" } = classDetails;
+
+// console.log(name)
+// console.log(email)
+// console.log(time)
