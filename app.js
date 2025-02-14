a0c550a0b156b25c4d457907c9d5ead379fc9ea3
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-02-14 23:06:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5359,3 +5359,31 @@ const filterSearch = (value) => {
 // console.log(person.name)
 
 
+//function
+
+// function calculate(a, b) {
+//     const add = a + b;
+//     const subtract = a - b;
+//     const multiply = a * b;
+//     const divide = a / b;
+  
+//     return [add, subtract, multiply, divide];
+//   }
+  
+//   const [add, subtract, multiply, divide] = calculate(4, 7);
+  
+//   console.log("Sum: " + add);
+//   console.log("Difference " + subtract);
+//   console.log("Product: " + multiply);
+//   console.log("Quotient " + divide);
+
+// Ternary operator
+
+// if (authenticated) {
+//     console.log('home');
+//   } else {
+//     console.log('Login Page');
+//   }
+
+//   authenticated ?  console.log('home') : console.log('Login Page');
+
