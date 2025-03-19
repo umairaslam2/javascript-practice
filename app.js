26fd990a895de2f921af7d90ced802bd1a7587ff
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-03-20 01:52:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6307,6 +6307,23 @@ const filterSearch = (value) => {
 
 // export { getDate, getTime, appName };
 
+// const fizzbuzz = (sNum, eNum) => {
+//   let arr = [];
+//   for (let i = sNum; i <= eNum; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       arr.push("fizzbuzz");
+//     } else if (i % 3 === 0) {
+//       arr.push("fizz");
+//     } else if (i % 5 === 0) {
+//       arr.push("buzz");
+//     } else {
+//       arr.push(i);
+//     }
+//   }
+//   return arr;
+// };
+
+
 
 //const promiseOne = new Promise(function(resolve, reject){
   //Do an async task
