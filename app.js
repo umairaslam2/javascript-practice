34c88b2d6c26fa36804486b4ec008bed7095a82b
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-03-21 21:52:49 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6306,3 +6306,18 @@ const filterSearch = (value) => {
 
 
 // export { getDate, getTime, appName };
+
+
+//const promiseOne = new Promise(function(resolve, reject){
+  //Do an async task
+  // DB calls, cryptography, network
+//   setTimeout(function(){
+//       console.log('Async task is compelete');
+//       resolve()
+//   }, 1000)
+// })
+
+// promiseOne.then(function(){
+//   console.log("Promise consumed");
+// })
+
