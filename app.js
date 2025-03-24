b6b34a5b38c915b9be128cc8cd81a1b1e5451c42
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-03-24 22:18:56 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6331,12 +6331,33 @@ const filterSearch = (value) => {
 //   console.log("Async 2 resolved");
 // })
 
-const promiseThree = new Promise(function(resolve, reject){
-  setTimeout(function(){
-      resolve({username: "Chai", email: "chai@example.com"})
-  }, 1000)
-})
+// const promiseThree = new Promise(function(resolve, reject){
+//   setTimeout(function(){
+//       resolve({username: "Chai", email: "chai@example.com"})
+//   }, 1000)
+// })
 
-promiseThree.then(function(user){
-  console.log(user);
-})
+// promiseThree.then(function(user){
+//   console.log(user);
+// })
+
+// const promiseFour = new Promise(function(resolve, reject){
+//   setTimeout(function(){
+//       let error = true
+//       if (!error) {
+//           resolve({username: "hitesh", password: "123"})
+//       } else {
+//           reject('ERROR: Something went wrong')
+//       }
+//   }, 1000)
+// })
+
+// promiseFour
+// .then((user) => {
+//   console.log(user);
+//   return user.username
+// }).then((username) => {
+//   console.log(username);
+// }).catch(function(error){
+//   console.log(error);
+// }).finally(() => console.log("The promise is either resolved or rejected"))
