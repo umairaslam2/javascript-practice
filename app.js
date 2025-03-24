10d6130a235c47276e79a77eab85241fc01ea4a5
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-03-25 02:04:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6378,3 +6378,50 @@ const filterSearch = (value) => {
 // }).catch(function(error){
 //   console.log(error);
 // }).finally(() => console.log("The promise is either resolved or rejected"))
+
+const promiseFive = new Promise(function(resolve, reject){
+  setTimeout(function(){
+      let error = true
+      if (!error) {
+          resolve({username: "javascript", password: "123"})
+      } else {
+          reject('ERROR: JS went wrong')
+      }
+  }, 1000)
+});
+
+async function consumePromiseFive(){
+  try {
+      const response = await promiseFive
+      console.log(response);
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+// fetch('https://api.github.com/users/hiteshchoudhary')
+// .then((response) => {
+//   return response.json()
+// })
+// .then((data) => {
+//   console.log(data);
+// })
+// .catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
