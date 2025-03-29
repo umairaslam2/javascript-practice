0c23b8867bbce150235d9978917b3c2e0395d6d6
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-03-29 16:45:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6469,3 +6469,33 @@ The new object is returned: After the constructor function has been called, if i
 
 */
 
+// let myName = "hitesh     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+// let myHeros = ["thor", "spiderman"]
+
+
+// let heroPower = {
+//     thor: "hammer",
+//     spiderman: "sling",
+
+//     getSpiderPower: function(){
+//         console.log(`Spidy power is ${this.spiderman}`);
+//     }
+// }
+
+// Object.prototype.hitesh = function(){
+//     console.log(`hitesh is present in all objects`);
+// }
+
+// Array.prototype.heyHitesh = function(){
+//     console.log(`Hitesh says hello`);
+// }
+
+// heroPower.hitesh()
+// myHeros.hitesh()
+// myHeros.heyHitesh()
+// heroPower.heyHitesh()
