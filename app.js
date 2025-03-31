776589f813660e664fb03c296f4c7aa9e2ab6355
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-03-31 22:27:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6543,3 +6543,40 @@ The new object is returned: After the constructor function has been called, if i
 
 */
 
+// inheritance
+
+// const User = {
+//     name: "chai",
+//     email: "chai@google.com"
+// }
+
+// const Teacher = {
+//     makeVideo: true
+// }
+
+// const TeachingSupport = {
+//     isAvailable: false
+// }
+
+// const TASupport = {
+//     makeAssignment: 'JS assignment',
+//     fullTime: true,
+//     __proto__: TeachingSupport
+// }
+
+// Teacher.__proto__ = User
+
+// // modern syntax
+// Object.setPrototypeOf(TeachingSupport, Teacher)
+
+// let anotherUsername = "ChaiAurCode     "
+
+// String.prototype.trueLength = function(){
+//     console.log(`${this}`);
+//     console.log(`True length is: ${this.trim().length}`);
+// }
+
+// anotherUsername.trueLength()
+// "hitesh".trueLength()
+// "iceTea".trueLength()
+
