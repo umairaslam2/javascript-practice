b4ca65d469b7cff6bffa11e5fb8933589ca69cb0
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-04-01 22:35:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6580,3 +6580,19 @@ The new object is returned: After the constructor function has been called, if i
 // "hitesh".trueLength()
 // "iceTea".trueLength()
 
+// function SetUsername(username){
+//     //complex DB calls
+//     this.username = username
+//     console.log("called");
+// }
+
+// function createUser(username, email, password){
+//     SetUsername.call(this, username)
+   
+//     this.email = email
+//     this.password = password
+// }
+
+// const chai = new createUser("chai", "chai@fb.com", "123")
+// console.log(chai);
+
