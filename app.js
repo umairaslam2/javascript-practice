28f6a016b4cc18f0c032ddc176dc3fd350fdc6c4
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-04-05 22:49:42 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6612,3 +6612,34 @@ The new object is returned: After the constructor function has been called, if i
 // const chai = new createUser("chai", "chai@fb.com", "123")
 // console.log(chai);
 
+// class User {
+//   constructor(username){
+//       this.username = username
+//   }
+
+//   logMe(){
+//       console.log(`USERNAME is ${this.username}`);
+//   }
+// }
+
+// class Teacher extends User{
+//   constructor(username, email, password){
+//       super(username)
+//       this.email = email
+//       this.password = password
+//   }
+
+//   addCourse(){
+//       console.log(`A new course was added by ${this.username}`);
+//   }
+// }
+
+// const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+// chai.logMe()
+// const masalaChai = new User("masalaChai")
+
+// masalaChai.logMe()
+
+// console.log(chai instanceof User);
+
