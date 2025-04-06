0a036df1557727e5b7eff44a008ef603f4a6769d
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-04-06 22:56:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6643,3 +6643,38 @@ The new object is returned: After the constructor function has been called, if i
 
 // console.log(chai instanceof User);
 
+// const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+// const chai = {
+//     name: 'ginger chai',
+//     price: 250,
+//     isAvailable: true,
+
+//     orderChai: function(){
+//         console.log("chai nhi bni");
+//     }
+// }
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+// Object.defineProperty(chai, 'name', {
+//     //writable: false,
+//     enumerable: true,
+    
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+// for (let [key, value] of Object.entries(chai)) {
+//     if (typeof value !== 'function') {
+        
+//         console.log(`${key} : ${value}`);
+//     }
+// }
+
