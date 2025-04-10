09ae64e4fc354218ddccb9f71844ad35f591ab52
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-04-10 23:59:24 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6678,3 +6678,48 @@ The new object is returned: After the constructor function has been called, if i
 //     }
 // }
 
+// ES6
+
+// class User {
+//   constructor(username, email, password){
+//       this.username = username;
+//       this.email = email;
+//       this.password = password
+//   }
+
+//   encryptPassword(){
+//       return `${this.password}abc`
+//   }
+//   changeUsername(){
+//       return `${this.username.toUpperCase()}`
+//   }
+
+// }
+
+// const chai = new User("chai", "chai@gmail.com", "123")
+
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUsername());
+
+// // behind the scene
+
+// function User(username, email, password){
+//   this.username = username;
+//   this.email = email;
+//   this.password = password
+// }
+
+// User.prototype.encryptPassword = function(){
+//   return `${this.password}abc`
+// }
+// User.prototype.changeUsername = function(){
+//   return `${this.username.toUpperCase()}`
+// }
+
+
+// const tea = new User("tea", "tea@gmail.com", "123")
+
+// console.log(tea.encryptPassword());
+// console.log(tea.changeUsername());
+
+
