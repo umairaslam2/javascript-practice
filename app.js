ced2df6a1bf5607fb4010a7d07c837b5c2858d5e
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-04-13 19:39:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6739,3 +6739,22 @@ The new object is returned: After the constructor function has been called, if i
 // const tea = Object.create(User)
 // console.log(tea.email);
 
+// const user = {
+//   username: "hitesh",
+//   loginCount: 8,
+//   signedIn: true,
+
+//   getUserDetails: function(){
+//       //console.log("Got user details from database");
+//       // console.log(`Username: ${this.username}`);
+//       console.log(this);
+//   }
+
+// }
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
