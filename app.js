9b31456992053f32fb725163508e0625d1e1eca6
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-04-14 19:40:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6758,3 +6758,21 @@ The new object is returned: After the constructor function has been called, if i
 //console.log(user.getUserDetails());
 // console.log(this);
 
+// function User(username, loginCount, isLoggedIn){
+//   this.username = username;
+//   this.loginCount = loginCount;
+//   this.isLoggedIn = isLoggedIn
+
+//   this.greeting = function(){
+//       console.log(`Welcome ${this.username}`);
+
+//   }
+
+//   return this
+// }
+
+// const userOne = new User("hitesh", 12, true)
+// const userTwo = new User("ChaiAurCode", 11, false)
+// console.log(userOne.constructor);
+//console.log(userTwo);
+
