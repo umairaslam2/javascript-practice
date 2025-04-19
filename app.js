ab2b2b39dd279d86885dd920948ccb2cc47b2604
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-04-19 19:41:32 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6776,3 +6776,30 @@ The new object is returned: After the constructor function has been called, if i
 // console.log(userOne.constructor);
 //console.log(userTwo);
 
+// function User(email, password){
+//   this._email = email;
+//   this._password = password
+
+//   Object.defineProperty(this, 'email', {
+//       get: function(){
+//           return this._email.toUpperCase()
+//       },
+//       set: function(value){
+//           this._email = value
+//       }
+//   })
+//   Object.defineProperty(this, 'password', {
+//       get: function(){
+//           return this._password.toUpperCase()
+//       },
+//       set: function(value){
+//           this._password = value
+//       }
+//   })
+
+// }
+
+// const chai = new User("chai@chai.com", "chai")
+
+// console.log(chai.email);
+
