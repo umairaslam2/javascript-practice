b1507e2bd9f97501988b9e9597a531e1e0c4757e
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-04-27 23:41:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6875,3 +6875,37 @@ The new object is returned: After the constructor function has been called, if i
 // Encapsulation
 // Inheritance
 // Polymorphism
+
+// console.log("hello world 1");
+// console.log("hello world 2");
+// console.log("hello world 3");
+// setTimeout(() => {
+//   console.log("hello world 4");
+// }, 0);
+// console.log("hello world 5");
+
+// settimeout
+// setinterval
+// promises
+// fetch
+// async await
+// axios
+
+// const data = dabba ma sa data mangwaya
+// console.log(data)
+
+// const data = datamangwalo
+// setTimeout(()=>{
+//     console.log(data)
+// } , 1000)
+
+//console.log("hello world!");
+
+// pending
+// Fullfilled
+// rejected
+
+// .then
+// .catch
+// const num = 1;
+
