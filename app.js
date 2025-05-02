ad92cac81fe7f120137eec13d976ac2181eba68f
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2025-05-02 23:47:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6989,3 +6989,31 @@ The new object is returned: After the constructor function has been called, if i
 // hello();
 
 // console.log("hello world!");
+
+
+const div = document.querySelector(".container");
+
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res.products);
+    res.products.map((item) => {
+      div.innerHTML += `<div class="card">
+            <img src=${item.thumbnail}
+                alt="thumbnail">
+            <h1>${item.title}</h1>
+            <p>${item.description.slice(0, 20)}...</p>
+            <p>Price: $${item.price}</p>
+            <button onclick="showMore(${item.id})">See more</button>
+        </div>`;
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const showMore = (id) => {
+  console.log(id);
+  localStorage.setItem("id", id);
+  window.location = "singleProduct.html";
+};
